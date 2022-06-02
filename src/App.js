335aa2d3cb2b087b2d33144168e26c1a3a6d7160
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-02 15:02:19 -0500

    chore: create base element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Products from './components/Products'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  layout: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}
+
+class App extends Component {
+  state = {
+    products: [
+      { name: 'Laptop', price: '$25 000.00', img: '/products/laptop.png' },
+      { name: 'Motherboard', price: '$3 000.00', img: '/products/motherboard.png' },
+      { name: 'Video card', price: '$15 000.00', img: '/products/videocard.png' },
+      { name: 'Monitor', price: '$12 000.00', img: '/products/monitor.png' },
+      { name: 'Power supply', price: '$4 500.00', img: '/products/power-supply.png' },
+      { name: 'Router', price: '$2 500.00', img: '/products/router.png' },
+      { name: 'Gabinet', price: '$3 500.00', img: '/products/gabinet.png' },
+      { name: 'Keyboard', price: '$3 000.00', img: '/products/keyboard.png' },
+      { name: 'Mouse', price: '$2 000.00', img: '/products/mouse.png' },
+    ]
+  }
+
+  render() {
+    return (
+      <div style={styles.layout}>
+        <Products
+          addToCart = {() => console.log('No hace nada')}
+          products = {this.state.products}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
