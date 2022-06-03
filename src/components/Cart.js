8f3a135b3a2363dd1f4c6a69ef2681ba637da6a2
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-03 11:39:18 -0500

    chore: create cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,44 @@
+import { Component } from "react";
+import BubbleAlert from './BubbleAlert';
+import DetailsCart from './DetailsCart';
+
+const styles = {
+  cart: {
+    backgroundColor: '#6D28D9',
+    color: 'white',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '3px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    fontWeight: '600',
+  },
+  bubble: {
+   position: 'relative' ,
+   left: 12,
+   top: 20,
+  }
+}
+
+class Cart extends Component {
+  render() {
+    const { cart, isCartVisible, showCart } = this.props
+    const quantity = cart.reduce((acc, el) => acc + el.quantity, 0)
+    return (
+      <div>
+        <span style={styles.bubble}>
+          { quantity !== 0
+            ? <BubbleAlert value={quantity} />
+            : null
+          }
+        </span>
+        <button onClick={showCart} style={styles.cart}>
+          Shopping cart
+        </button>
+        {isCartVisible ? <DetailsCart cart={cart}/> : null}
+      </div>
+    )
+  }
+}
+
+export default Cart;
